ca16ae0678456b99ba796a3122aa105e4c26be23
Sushrut Warekar <sushrut.warekar@gmail.com>
2025-07-02 19:35:34 -0500

    naive hsd implementation

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ STD_DECK = ['2', '2', '2', '2', '3', '3', '3', '3',
         '10', '10', '10', '10', 'J', 'J', 'J', 'J',
         'Q', 'Q', 'Q', 'Q', 'K', 'K', 'K', 'K',
         'A', 'A', 'A', 'A']
+VALUE = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
+         '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11}
 
 
 class Player:
@@ -18,6 +20,7 @@ class Player:
         self.balance = deposit
         self.bet = 0
         self.hand = []
+        self.action = 'z'
 
 
 def clearDisplay():
@@ -41,10 +44,70 @@ def printHands(dealer, players):
 
 
 def hasBlackjack(hand):
-    if len(hand) == 2 and 'A' in hand and any(card in hand for card in ['10', 'J', 'Q', 'K']):
+    if sum(VALUE[card] for card in hand) == 21:
         return True
     return False
 
+
+def playerActions(dealer, players, deck, hole_card):
+    clearDisplay()
+    printHands(dealer, players)
+    left_standing = len(players)
+    for p in players:
+        print("================================")
+        if hasBlackjack(p.hand):
+            print(f"{p.name} has BLACKJACK!")
+            continue
+        while True:
+            action = input(f"{p.name}, choose your action (h: hit, s: stand, d: double down): ")[0].lower()
+            # player hits
+            if action == 'h':
+                ind = random.randint(0, len(deck)-1)
+                p.hand.append(deck[ind])
+                del deck[ind]
+                print(f"{p.name} hits and draws {p.hand[-1]}")
+                if hasBlackjack(p.hand):
+                    print(f"{p.name} has BLACKJACK!")
+                    break
+                elif sum(VALUE[card] for card in p.hand) > 21:
+                    print(f"{p.name} busts! You lose ${p.bet}")
+                    p.balance -= p.bet
+                    p.bet = 0
+                    left_standing -= 1
+                    break
+                #
+                # NEED TO BE ABLE TO HIT REPEATEDLY
+                #
+
+            # player stands
+            elif action == 's':
+                print(f"{p.name} stands with {sum(VALUE[card] for card in p.hand)}")
+                break
+            #player doubles down
+            elif action == 'd':
+                if p.balance < p.bet*2:
+                    print("!-- Not enough balance to double down --!")
+                else:
+                    p.bet *= 2
+                    ind = random.randint(0, len(deck)-1)
+                    p.hand.append(deck[ind])
+                    del deck[ind]
+                    break
+            else:
+                print("!-- Invalid action. Please choose again --!")
+    print("================================\nAll players have acted. Dealer's hole card is .")
+    dealer[1] = hole_card
+    printHands(dealer, players)
+    dealerActions(dealer, players, deck)
+    
+    
+    
+    # call dealerActions() after all players have acted
+    # dealerActions() takes care of returning bets for
+        # push
+        # player blackjack
+        # dealer busts
+    # inside dealerActions(), call nextRound() after dealer's turn is over
 
 
 def startGame(players):
@@ -78,18 +141,23 @@ def startGame(players):
         del deck[ind]
     
     # handle dealer blackjack
-    dealer[0] = 'A'
-    hole_card = 'J'    
     if hasBlackjack([dealer[0], hole_card]):
         dealer[1] = hole_card
         dealerBJ(dealer, players)
         return
-    clearDisplay()
-    printHands(dealer, players)
+    playerActions(dealer, players, deck, hole_card)
 
-# TO DO: Implement player actions (hit, stand, double down, split)
+# TO DO:
+# Implement player actions (hit, stand, double down, split)
+# Implement dealer actions (hit until 17 or higher)
 
 def nextRound(dealer, players):
+    if not players:
+        print("No players left. Exiting game.")
+        return
+    for p in players:
+        p.hand = []
+        p.bet = 0
     print("================================\nPress ENTER to continue to next round (q to quit): ", end='')
     choice = input()
     if len(choice) == 0:
@@ -106,6 +174,7 @@ def nextRound(dealer, players):
 def dealerBJ(dealer, players):
     clearDisplay()
     print("_________________________\n\ Dealer has BLACKJACK! /\n -----------------------")
+    broke = []
     for p in players:
         print(f"{p.name}: | ", end='')
         for card in p.hand:
@@ -117,6 +186,13 @@ def dealerBJ(dealer, players):
             p.balance -= p.bet
             print(f" --> Lost ${p.bet} | New Balance: ${p.balance}")
         p.bet = 0
+    
+    for p in players:
+        if p.balance <= 0:
+            print(f"!-- {p.name} is out of money --!")
+            broke.append(p)
+    for p in broke:
+        players.remove(p)
     nextRound(dealer, players)
 
 
